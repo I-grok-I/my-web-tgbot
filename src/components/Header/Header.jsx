f3ef37b5746e14fd87312c74a8c554b3948581b6
Ramzan <117625419+I-grok-I@users.noreply.github.com>
2022-12-14 00:06:39 +0500

    one else

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import Button from '../Button/Button'
-import { useTelegram } from '../hooks/useTelegram';
+import Button from '../Button/Button';
+import {useTelegram} from '../hooks/useTelegram';
+
  const Header = () => {
-    const {user, onClose} = useTelegram()
+    const {user, onClose} = useTelegram();
 
 
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     );
